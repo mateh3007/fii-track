636cf08eb34ee130fabe953e8a6722ef883c9870
Matheus Reis <mateh11233@gmail.com>
2024-11-17 00:31:00 -0300

    feat: create delete-user use-case

---
+++ src/application/use-cases/users/delete-user/delete-user.use-case.ts
@@ -0,0 +1,17 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { IUserRepository } from 'src/domain/interfaces/user/user.interface';
+
+@Injectable()
+export class DeleteUserService {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  async handle(id: number): Promise<void> {
+    const userAlreadyExists = await this.userRepository.findById(id);
+
+    if (!userAlreadyExists) {
+      throw new NotFoundException('Company not found');
+    }
+
+    return await this.userRepository.delete(userAlreadyExists.id);
+  }
+}
--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CreateUserUseCase } from 'src/application/use-cases/users/create-user/create-user.use-case';
+import { UpdateUserUseCase } from 'src/application/use-cases/users/update-user/update-user.use-case';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [CreateUserUseCase],
+  providers: [CreateUserUseCase, UpdateUserUseCase],
 })
 export class UserModule {}
